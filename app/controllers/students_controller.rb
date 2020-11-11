2dad33b1e8dce5a9f385ba49ea137647ab454559
Ninjosh2 <ninjosh2@gmail.com>
2020-11-11 04:37:10 -0600

    adds to students controllers and views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,32 @@
 class StudentsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_student, only: [:show, :edit, :update, :destroy]
 
     def index
+        @students = current_user.students
+    end
+
+    def show
+        @student = current_user.students.find(params[:id])
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
 
     end
 end
