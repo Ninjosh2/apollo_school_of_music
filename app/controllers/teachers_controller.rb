ceb204742c9cb5f2e82240ac77a1fe30de6dbddb
Ninjosh2 <ninjosh2@gmail.com>
2020-11-12 05:02:31 -0600

    adds to the teachers controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,41 @@
 class TeachersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+    def index
+        @teachers = Teachers.all
+    end
+    
+    def show
+        set_teacher
+    end
+    
+    def new
+    
+    end
+    
+    def create
+        
+    end
+    
+    def edit
+    
+    end
+    
+    def update
+    
+    end
+    
+    def destroy
+    
+    end
+
+    private
+
+    def set_teacher
+        @teacher = Teacher.find(params[:id])
+    end
+
+    def teacher_params
+        params.require(:teacher).permit(:name, :phone_number, :email, :instrument)
+    end
 end
