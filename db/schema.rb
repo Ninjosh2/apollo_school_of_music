3362917e3bec939dc1ed19f4516a3ba17cb3dc12
Ninjosh2 <ninjosh2@gmail.com>
2020-11-14 03:47:52 -0600

    works on implementing oauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_190947) do
+ActiveRecord::Schema.define(version: 2020_11_14_094328) do
 
   create_table "lessons", force: :cascade do |t|
     t.integer "teacher_id", null: false
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 2020_11_10_190947) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "full_name"
+    t.string "avatar_url"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
