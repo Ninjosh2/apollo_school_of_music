254db6b25785003d92db2a57a0b4cbb7a2d8a855
Ninjosh2 <ninjosh2@gmail.com>
2020-11-16 03:38:55 -0600

    add oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,20 @@ class User < ApplicationRecord
   has_many :teachers, through: :students
   has_many :lessons, through: :students
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth_2]
 
-  # def self.from_google(uid:, email:, full_name:, avatar_url:)
-  #   user = User.find_or_create_by(email: email) do |u|
-  #     u.uid = uid
-  #     u.full_name = full_name
-  #     u.avatar_url = avatar_url
-  #     u.password = SecureRandom.hex
-  #   end
-  #     user.update(uid: uid, full_name: full_name, avatar_url: avatar_url)
-  #     user
-  # end
-
+  def self.from_google(uid:, email:, full_name:, avatar_url:)
+    if user = User.find_by(email: email)
+      user.update(uid: uid, full_name: full_name, avatar_url: avatar_url) unless user.uid.present?
+      user
+    else
+      User.create(
+        email: email,
+        uid: uid,
+        full_name: full_name,
+        avatar_url: avatar_url,
+        password: SecureRandom.hex
+      )
+    end
+  end
 end
