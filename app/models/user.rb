55f088fdd2a48bed672082efd2856b45e6bbaf09
Ninjosh2 <ninjosh2@gmail.com>
2020-11-17 19:27:51 -0600

    adding oauth and bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :students
+  has_many :teachers
   has_many :teachers, through: :students
   has_many :lessons, through: :students
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2], authentication_keys: [:login], reset_password_keys: [:login]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(uid:, email:, full_name:, avatar_url:)
     if user = User.find_by(email: email)
